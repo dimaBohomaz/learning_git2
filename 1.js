f8e9196cdfd4353083a5016223dd60ab745a4c34
Dmytro Bohomaz <bogomaz75@i.ua>
2020-01-22 13:48:10 +0200

    created figure1

--- 1.js
+++ 1.js
@@ -6,6 +6,13 @@ class Figure {
     }
     getInfo() {
         return `type: ${this.type} size : ${this.size} color: ${this.color}`;
-    }
+    };
+    getOne() {
+        return 1
+    };
+
 }
 let figure1 = require('./data');
+let square1 = new Figure(figure1.type, figure1.size, figure1.color);
+console.log(Figure.prototype.getOne());
+console.log(Object.getOwnPropertyNames(figure1));
